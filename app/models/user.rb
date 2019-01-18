73074ac88aa8fb5363f642216ef202bf9533fc31
Virginia Dooley <vcdooley@gmail.com>
2019-01-18 17:41:43 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
+# require 'bcrypt'
+
 class User < ActiveRecord::Base
+
+	# include BCrypt
+
+	has_secure_password
+	# #
+	# def password
+  #   @password ||= Password.new(password_hash)
+  # end
+	#
+  # def password=(new_password)
+  #   @password = Password.create(new_password)
+  #   self.password_hash = @password
+  # end
 end
